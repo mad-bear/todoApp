bfbc3d64ddd3ac693850fad69993cdc0e3549eed
mh <mre.ezboy3@gmail.com>
2017-07-03 14:50:02 +0330

    final

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const URL = 'mongodb://mrez:7582062@ds145292.mlab.com:45292/mydb' | 'mongodb://localhost:27017/TodoApp';
+const URL = 'mongodb://mrez:7582062@ds145292.mlab.com:45292/mydb';
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGO_ADD || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(URL);
 console.log('on URL: ', URL);
 
 module.exports = {
